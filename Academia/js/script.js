4b0674c3236a689e81c847f93a8b1826b2d33f71
Gustavo Teodozio <108893339+GustavoTeodozio@users.noreply.github.com>
2023-05-26 15:09:51 -0300

    Update script.js
    Arrumei o javascript

--- Academia/js/script.js
+++ Academia/js/script.js
@@ -1,7 +1,7 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-	menu.classList.toggle('bx-x');
-	navbar.classList.toggle('open');
-}
+$(document).ready(function() {
+	$('#menu-icon').click(function() {
+	  $(this).toggleClass('bx-x');
+	  $('#navbar').toggleClass('open');
+	});
+  });
+  
